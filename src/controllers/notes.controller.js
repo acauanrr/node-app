e32de0e3cd173c1695db6f1eb04ec88df22fbbe3
Acauan Ribeiro <acauan.ribeiro@gmail.com>
2024-02-09 19:41:20 -0400

    first commit

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,49 +1,50 @@
 const notesCtrl = {};
 
-const Note = require('../models/Note');
+const Note = require("../models/Note");
 
 notesCtrl.renderNoteForm = (req, res) => {
-    res.render('notes/new-note');
-}
+  res.render("notes/new-note");
+};
 
 notesCtrl.createNewNode = async (req, res) => {
-    const {title, description} = req.body;
-    const newNote = new Note({title, description});
-    newNote.user = req.user.id; // guardando dados do usuario em cada nota
-    await newNote.save();
-    req.flash('success_msg', 'Note Added Successfully');
-    res.redirect('/notes');
+  const { title, description } = req.body;
+  const newNote = new Note({ title, description });
+  newNote.user = req.user.id; // guardando dados do usuario em cada nota
+  await newNote.save();
+  req.flash("success_msg", "Note Added Successfully");
+  res.redirect("/notes");
 };
 
 notesCtrl.renderNotes = async (req, res) => {
-    const notes = await Note.find({user: req.user.id}).sort({createdAt: 'desc'}); //Mongodb
-    // console.log(req.user);
-    res.render('notes/all-notes', { notes, nameUser: req.user.name });
+  const notes = await Note.find({ user: req.user.id }).sort({
+    createdAt: "desc",
+  }); //Mongodb
+  // console.log(req.user);
+  res.render("notes/all-notes", { notes, nameUser: req.user.name });
 };
 
 notesCtrl.renderEditForm = async (req, res) => {
-    const note = await Note.findById(req.params.id)
-    if(note.user != req.user.id) {
-        req.flash('error_msg', 'Not Authorized');
-        return res.redirect('/notes');
-    }
-    res.render('notes/edit-note', { note })
+  const note = await Note.findById(req.params.id);
+  if (note.user != req.user.id) {
+    req.flash("error_msg", "Not Authorized");
+    return res.redirect("/notes");
+  }
+  res.render("notes/edit-note", { note });
 };
 
 notesCtrl.updateNote = async (req, res) => {
-    // console.log(req.body);
-    const {title, description} = req.body;
-    await Note.findByIdAndUpdate(req.params.id, {title, description});
-    req.flash('success_msg', 'Note Updated Successfully');
-    res.redirect('/notes');
+  // console.log(req.body);
+  const { title, description } = req.body;
+  await Note.findByIdAndUpdate(req.params.id, { title, description });
+  req.flash("success_msg", "Note Updated Successfully");
+  res.redirect("/notes");
 };
 
 notesCtrl.deleteNote = async (req, res) => {
-    // res.send('Delete note');
-    await Note.findByIdAndDelete(req.params.id); //Mongodb
-    req.flash('success_msg', 'Note Deleted Successfully');
-    res.redirect('/notes');
-
+  // res.send('Delete note');
+  await Note.findByIdAndDelete(req.params.id); //Mongodb
+  req.flash("success_msg", "Note Deleted Successfully");
+  res.redirect("/notes");
 };
 
 module.exports = notesCtrl;
--- src/database.js
+++ src/database.js
@@ -1,12 +1,47 @@
-const mongoose = require('mongoose');
+// ------------------ MONGODB ---------------------
+const mongoose = require("mongoose");
 
-// const MONGODB_URI = 'mongodb://localhost/notes-app';
+const MONGODB_URI = "mongodb://localhost/notes-app"; // <-- VOCÊ DEVE TER UM MONGODB RODANDO LOCALMENTE!!!!
 
-const MONGODB_URI = 'mongodb+srv://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_HOST+'/'+process.env.MONGODB_DATABASE+'?retryWrites=true&w=majority';
+// const MONGODB_URI = 'mongodb+srv://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_HOST+'/'+process.env.MONGODB_DATABASE+'?retryWrites=true&w=majority';
 
-mongoose.connect(MONGODB_URI, {
+mongoose
+  .connect(MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-})
-.then(db => console.log("Mongodb is connected to", db.connection.host))
-.catch(err => console.log(err));
+  })
+  .then((db) => console.log("Mongodb is connected to", db.connection.host))
+  .catch((err) => console.log(err));
+
+// ------------------ NEO4J ---------------------
+//Configuração neo4j -- https://adamcowley.co.uk/javascript/using-the-neo4j-driver-with-nodejs/
+// const neo4j = require("neo4j-driver");
+// const driver = new neo4j.driver(
+//   "neo4j://localhost:7687",
+//   neo4j.auth.basic("neo4j", "node-app")
+// ); //("usuario", "senha")
+// console.log(`Neo4j Database running at localhost:7687`);
+// const session = driver.session({
+//   database: "neo4j", // <-- Connect to the database `neo4j`
+// });
+
+// const neo = async () => {
+//   const results = await session.run("MATCH (n:Pessoa) RETURN n", {});
+//   session.close();
+
+//   //console.log("RESULT", !results ? 0 : results.records.length);
+
+//   const pessoas = [];
+
+//   results.records.forEach((res) => {
+//     //    console.log(res.get(0).properties)
+//     pessoas.push({
+//       nome: res.get(0).properties.nome,
+//       idade: res.get(0).properties.idade.low,
+//     });
+//   });
+
+//   console.log(pessoas);
+// };
+
+// neo();
